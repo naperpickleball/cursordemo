931707a0d2d649c74fa0fec319e0aa594ad478a3
naperPickleball <naperpickleball@gmail.com>
2025-05-10 23:17:05 -0500

    Add new version component

---
+++ src/pages/NewVersion.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NewVersion.css';
+
+const NewVersion = () => {
+  return (
+    <div className="new-version">
+      <h1>New Version of the App</h1>
+      <p>This is a separate version of the application.</p>
+      <p>You can make changes here without affecting the original code.</p>
+    </div>
+  );
+};
+
+export default NewVersion; 
